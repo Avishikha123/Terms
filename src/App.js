4f0fe2e9973f452267e8401f04e47fe8fda13333
Avishikha123 <avishikha.pandey@tavant.com>
2021-12-11 20:24:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+
+import Faq from './Faq'
+
+const App = () => {
+  
+
+  return (
+    <>
+    <Faq/>
+    </>
+    // <main>
+    //   <div className='container'>
+    //     <h3>frequently asked questions</h3>
+    //     <section className='info'>
+    //       {questions.map((question) => (
+    //         <SingleQuestion key={question.id} {...question} />
+    //       ))}
+    //     </section>
+    //   </div>
+    // </main>
+  )
+}
+
+export default App
